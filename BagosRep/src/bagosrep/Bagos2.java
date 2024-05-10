a6a1d3735c88c1a12d25f3892edc853e5cac0370
Dyltiadasdad <dasdasdsadasd@gmail.com>
2024-05-10 12:58:51 -0500

    Segunda vez with github

--- BagosRep/src/bagosrep/Bagos2.java
+++ BagosRep/src/bagosrep/Bagos2.java
@@ -1,5 +1,9 @@
 package bagosrep;
 
 public class Bagos2 {
-    
+     public static void main(String[] args) {
+ 
+        System.out.println("Hola bbsitas brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+        
+    }
 }
