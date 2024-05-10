e486d4d078c66ec3bbf4d431f901563840c0b381
Dyltiadasdad <dasdasdsadasd@gmail.com>
2024-05-10 12:45:47 -0500

    primera vez with github

---
+++ BagosRep/src/bagosrep/BagosRep.java
@@ -0,0 +1,11 @@
+package bagosrep;
+
+public class BagosRep {
+
+    public static void main(String[] args) {
+ 
+        System.out.println("Hola bbsitas brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+        
+    }
+    
+}
